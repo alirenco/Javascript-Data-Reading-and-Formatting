788d383eb494feb2cb9dc17c6ca61d90d64b9e0c
Alisha Coffey <alisha.r.coffey@gmail.com>
2021-03-16 21:02:45 -0700

    Adding finished homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,46 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Build Table for sightings
 var tbody = d3.select("tbody");
 
-console.log(data);
+console.log(tableData);
 
-data.forEach(function(sighting) {
-    console.log(sighting);
-  });
-
-
-//   Append tr to each  
-data.forEach(function(sighting) {
-    console.log(sighting);
-    var row = tbody.append("tr");
-  });
-
-//  print each sighting to console
-data.forEach(function(sighting) {
-    console.log(sighting);
-    var row = tbody.append("tr");
-  
-    Object.entries(sighting).forEach(function([key, value]) {
-      console.log(key, value);
-    });
-  });
 
 //  Make one cell per UFO sighting
-// data.forEach(function(sighting) {
-//     console.log(sighting);
-//     var row = tbody.append("tr");
-  
-//     Object.entries(sighting).forEach(function([key, value]) {
-//       console.log(key, value);      
-//       var cell = row.append("td"); // Append a cell to the row for each value
-//     });
-//   });
-
-
-// Update text in each cell
-data.forEach(function(sighting) {
+tableData.forEach(function(sighting) {
     console.log(sighting);
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(function([key, value]) {
@@ -50,3 +18,40 @@ data.forEach(function(sighting) {
     });
   });
   
+
+
+  // Event Listener - filter by date
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue); // filter by date
+  console.log(filteredData);
+
+  //  Hide Full Table
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+
+  for (n = 1; n < tr.length; n++) {
+        tr[n].style.display = "none";
+  }
+
+  //  Print date-filtered results
+  filteredData.forEach(function(sighting) {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td"); // Append a cell to the row for each value
+      cell.text(value);
+    });
+  });
+
+}
+
+
